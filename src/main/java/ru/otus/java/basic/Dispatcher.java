11cf15a287e17b60a5f3a5160988f385b4be9ad8
xa86 <xa86@mail.ru>
2024-07-31 10:59:24 +0300

    Homework twenty-four basic implementation

--- src/main/java/ru/otus/java/basic/Dispatcher.java
+++ src/main/java/ru/otus/java/basic/Dispatcher.java
@@ -1,10 +1,14 @@
 package ru.otus.java.basic;
 
 
+import com.google.gson.Gson;
+import ru.otus.java.basic.app.ItemsRepository;
 import ru.otus.java.basic.processors.AnotherHelloWorldRequestProcessor;
 import ru.otus.java.basic.processors.CalculatorRequestProcessor;
+import ru.otus.java.basic.processors.CreateNewItemProcessor;
 import ru.otus.java.basic.processors.DefaultInternalServerErrorRequestProcessor;
 import ru.otus.java.basic.processors.DefaultNotFoundRequestProcessor;
+import ru.otus.java.basic.processors.GetAllItemsProcessor;
 import ru.otus.java.basic.processors.HelloWorldRequestProcessor;
 import ru.otus.java.basic.processors.RequestProcessor;
 
@@ -19,11 +23,17 @@ public class Dispatcher {
     private RequestProcessor defaultNotFoundRequestProcessor;
     private RequestProcessor defaultInternalServerErrorProcessor;
 
+    private ItemsRepository itemsRepository;
+
     public Dispatcher() {
+        this.itemsRepository = new ItemsRepository();
+
         this.processors = new HashMap<>();
-        this.processors.put("/", new HelloWorldRequestProcessor());
-        this.processors.put("/another", new AnotherHelloWorldRequestProcessor());
-        this.processors.put("/calculator", new CalculatorRequestProcessor());
+        this.processors.put("GET /", new HelloWorldRequestProcessor());
+        this.processors.put("GET /another", new AnotherHelloWorldRequestProcessor());
+        this.processors.put("GET /calculator", new CalculatorRequestProcessor());
+        this.processors.put("GET /items", new GetAllItemsProcessor(itemsRepository));
+        this.processors.put("POST /items", new CreateNewItemProcessor(itemsRepository));
 
         this.defaultNotFoundRequestProcessor = new DefaultNotFoundRequestProcessor();
         this.defaultInternalServerErrorProcessor = new DefaultInternalServerErrorRequestProcessor();
@@ -31,18 +41,20 @@ public class Dispatcher {
 
     public void execute(HttpRequest request, OutputStream out) throws IOException {
         try {
-            if (!processors.containsKey(request.getUri())) {
+            if (!processors.containsKey(request.getRoutingKey())) {
                 defaultNotFoundRequestProcessor.execute(request, out);
                 return;
             }
-            processors.get(request.getUri()).execute(request, out);
+            processors.get(request.getRoutingKey()).execute(request, out);
         } catch (BadRequestException e) {
             e.printStackTrace();
+            DefaultErrorDto defaultErrorDto = new DefaultErrorDto("CLIENT_DEFAULT_ERROR", e.getMessage());
+            String jsonError = new Gson().toJson(defaultErrorDto);
             String response = "" +
                     "HTTP/1.1 400 Bad Request\r\n" +
-                    "Content-Type: text/html\r\n" +
+                    "Content-Type: application/json\r\n" +
                     "\r\n" +
-                    "<html><body><h1>" + e.getMessage() + "</h1></body></html>";
+                    jsonError;
             out.write(response.getBytes(StandardCharsets.UTF_8));
         } catch (Exception e) {
             e.printStackTrace();
